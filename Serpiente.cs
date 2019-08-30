6bf5d7ec49e308a03eb93f22402fd215eed235d1
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2019-08-29 23:07:02 -0500

    Add files via upload

---
+++ Serpiente.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Serpiente : MonoBehaviour {
+
+    public GameObject Bloque;
+    public GameObject Escenario;
+    public int Ancho, Alto;
+
+    private void Awake()
+    {
+        CrearMuros();
+    }
+
+    private void CrearMuros()
+    {
+
+        for (int x = 0; x < Ancho; x++)
+        {
+            for (int y = 0; y < Alto; y++)
+            {
+                Vector3 posicion = new Vector3(x, y);
+                Instantiate(Bloque, posicion, Quaternion.identity, Escenario.transform);
+            }
+        }
+
+}
